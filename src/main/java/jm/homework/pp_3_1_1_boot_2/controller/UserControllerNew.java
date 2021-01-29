9b837af2a47976deb4657dea0cb708a6bd0f322d
Atiacht <atiacht@gmail.com>
2021-01-29 12:20:56 +0300

    #4

--- src/main/java/jm/homework/pp_3_1_1_boot_2/controller/UserControllerNew.java
+++ src/main/java/jm/homework/pp_3_1_1_boot_2/controller/UserControllerNew.java
@@ -61,51 +61,84 @@ public class UserControllerNew {
 //        return "user";
 //    }
 //
-    @RequestMapping(value = "/user-b/{id}", method = RequestMethod.DELETE)
+    @DeleteMapping(value = "/user-b/{id}")
     public String deleteUser(@PathVariable("id") long id, Model model) {
         userService.deleteUserById(id);
         return "redirect:/admin-b";
     }
-//
-//    @GetMapping(value = "/user/{id}")
-//    public String editUserFormSecond(@PathVariable("id") long id,
-//                                     Model model){
-//        if(!model.containsAttribute("user")){
-//            User user = userService.showById(id);
-//            model.addAttribute("user", user);
-//            Set<Role> rolesAll = roleService.getAllRoles();
-//            model.addAttribute("rolesAll", rolesAll);
-//        }
-//        return "editUserSecond";
-//    }
-//
+
     @PatchMapping(value = "/user-b/{id}")
-    public String getUserFormSecond(@ModelAttribute("user") @Valid User user,
-                                    BindingResult bindingResult,
+    public String getUserFormSecond(@ModelAttribute("user") User user,
                                     @PathVariable("id") long id,
                                     Model model,
                                     RedirectAttributes redirectAttributes) {
         boolean err = false;
         User oldUser = userService.showById(id);
 
+        boolean emailEmpty = user.getEmail().isEmpty();
+        boolean nameEmpty = user.getName().isEmpty();
+        boolean passwordEmpty = user.getPassword().isEmpty();
+
         if ((!oldUser.getEmail().equals(user.getEmail()))
                 && userService.isExistingUserByEmail(user.getEmail())) {
             redirectAttributes.addFlashAttribute("errorExist", "this email is already exist");
             err = true;
         }
-        if (err || bindingResult.hasErrors()) {
-            redirectAttributes.addFlashAttribute("user", user);
-            redirectAttributes.addFlashAttribute("org.springframework.validation.BindingResult.user", bindingResult);
-            redirectAttributes.addFlashAttribute("rolesAll", roleService.getAllRoles());
-            return String.format("redirect:/user-b/%d", user.getId());
+
+        if (err || emailEmpty || nameEmpty || passwordEmpty) {
+            redirectAttributes.addFlashAttribute("emailEmpty", emailEmpty);
+            redirectAttributes.addFlashAttribute("nameEmpty", nameEmpty);
+            redirectAttributes.addFlashAttribute("passwordEmpty", passwordEmpty);
+//            redirectAttributes.addFlashAttribute("rolesAll", roleService.getAllRoles());
+            return String.format("redirect:/admin-b");
         }
         userService.updateUser(user);
-        return String.format("redirect:/user-b/%d", user.getId());
+        return String.format("redirect:/admin-b");
     }
 
 
     @GetMapping(value = "/admin-b")
     public String getAdminPage(Model model) {
+        allNecessaryAddingToModel(model);
+        return "admin_b";
+    }
+
+    @GetMapping(value = "/user-b/{id}")
+    public String getUserPage(@PathVariable("id") long id, Model model) {
+        allNecessaryAddingToModel(model);
+        return "user_b";
+    }
+
+
+//    @PatchMapping("/user-b2/{id}")
+//    public String getCommonUserEdit(@PathVariable("id") long id,
+////                                    @RequestParam(value = "id", required = false) long id,
+////                                    @RequestParam(value = "email", required = false) String email,
+////                                    @RequestParam(value = "name", required = false) String name,
+////                                    @RequestParam(value = "age", required = false) String age,
+////                                    @RequestParam(value = "roles", required = false) List<Role> roles,
+//                                    @ModelAttribute("user") User user,
+//                                    Model model
+//                                    ) {
+//        System.out.println(user.getId());
+//        System.out.println(user.getEmail());
+//        System.out.println(user.getName());
+//        System.out.println(user.getAge());
+//        System.out.println(user.getRoles());
+//        System.out.println(user.getPassword());
+//
+//        return "redirect:/admin-b";
+//    }
+
+    private String getEmailByAuthenticatedUser(Authentication authentication) {
+        Object principal = authentication.getPrincipal();
+        if (principal instanceof UserDetails) {
+            return ((UserDetails)principal).getUsername();
+        }
+        return principal.toString();
+    }
+
+    private Model allNecessaryAddingToModel(Model model) {
         Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
 
         Set<String> roles = authentication.getAuthorities().stream()
@@ -123,64 +156,13 @@ public class UserControllerNew {
         List<User> listUsers = userService.allUsers();
 
         model.addAttribute("user", new User());
-
         model.addAttribute("rolesAll", roleService.getAllRoles());
         model.addAttribute("listUsers", listUsers);
         model.addAttribute("email", email);
         model.addAttribute("roles", roles);
         model.addAttribute("authUser", user);
-        return "admin_b";
-    }
 
-    @GetMapping(value = "/user-b/{id}")
-    public String getUserPage(@PathVariable("id") long id, Model model) {
-        Authentication authentication = SecurityContextHolder.getContext().getAuthentication();
-
-        Set<String> roles = authentication.getAuthorities().stream()
-                .map(GrantedAuthority::getAuthority).collect(Collectors.toSet());
-
-        String email;
-        Object principal = authentication.getPrincipal();
-        if (principal instanceof UserDetails) {
-            email = ((UserDetails)principal).getUsername();
-        } else {
-            email = principal.toString();
-        }
-
-        User user = (User) userService.findByEmail(email);
-        List<User> listUsers = userService.allUsers();
-
-        model.addAttribute("listUsers", listUsers);
-        model.addAttribute("email", email);
-        model.addAttribute("roles", roles);
-        model.addAttribute("authUser", user);
-
-        return "user_b";
-    }
-
-//    @GetMapping("/modal_edit_user/{id}")
-//    public String getCommonUserEdit(@PathVariable("id") long id, Model model) {
-//        return "common_user_edit";
-//    }
-
-    @PatchMapping("/user-b2/{id}")
-    public String getCommonUserEdit(@PathVariable("id") long id,
-//                                    @RequestParam(value = "id", required = false) long id,
-//                                    @RequestParam(value = "email", required = false) String email,
-//                                    @RequestParam(value = "name", required = false) String name,
-//                                    @RequestParam(value = "age", required = false) String age,
-//                                    @RequestParam(value = "roles", required = false) List<Role> roles,
-                                    @ModelAttribute("user") User user,
-                                    Model model
-                                    ) {
-        System.out.println(user.getId());
-        System.out.println(user.getEmail());
-        System.out.println(user.getName());
-        System.out.println(user.getAge());
-        System.out.println(user.getRoles());
-
-//        System.out.println(id + email + name + age);
-        return "redirect:/admin-b";
+        return model;
     }
 
 
